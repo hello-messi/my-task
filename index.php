989ea906c7dc882a873c86be3e17fba6f6904504
osama <osama@osama.osama>
2025-05-19 20:38:03 +0300

    adding my index.php file to my project

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+
+/**
+ * Write a program to find the sum of all numbers from 1 to 1000 that meet all of the following conditions:
+
+The number must be a multiple of 3 or 5.
+
+The number must not be a multiple of 7.
+
+The number must be greater than 50 and less than 500.
+ */
+
+// $sum = 0;
+// for ($i = 1;$i<=1000;$i++) {
+//   if(($i % 3=== 0 || $i % 5 === 0) && $i % 7 !== 0 && $i > 50 && $i < 500) {
+//     $sum += $i;
+//   }
+// }
+
+
+// echo $sum;
+
+
+
+// get the max and min of the array
+// $nums = [10, 33, 1222, 4444, 0, -1111];
+
+// $max = $nums[0];
+// $min = $nums[0];
+// $i = 1;
+// while($i < count($nums)) {
+//   if($nums[$i] > $max) {
+//     $max = $nums[$i];
+//   }
+
+//   if($nums[$i] < $min) {
+//     $min = $nums[$i];
+//   }
+
+//   $i++;
+// }
+
+
+// echo $max . " " . $min;
